587172e5f39de3f8277fe6be4d1322da67c3954d
ckollmer <ckollmer@rstudio.macalester.edu>
2020-10-14 11:20:08 -0500

    Final

--- app.r
+++ app.r
@@ -1,22 +1,32 @@
 library(shiny)
 library(tidyverse)
+library(dplyr)
+library(rsconnect)
 
 covid19 <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
 
 ui <- fluidPage(
   selectInput("state", 
               "State",
-              choices ),
+              choices = unique(covid19$state),
+              multiple = TRUE),    
+  submitButton(text = "View Plot"),
   plotOutput(outputId = "timeplot")
 )
 
 server <- function(input, output){
   output$timeplot <- renderPlot({
-    ggplot(aes(x = cases, y = weekly, color = state)) + 
-      scale_x_log10(label = scales::comma) + 
-      scale_y_log10(label = scales::comma) + 
-      geom_path() +
-      theme_minimal()
+    covid19 %>%
+      filter(cases >= 20,
+             state %in% input$state) %>%
+      group_by(state) %>% 
+      mutate(days_since_20 = row_number()) %>%
+    ggplot() + 
+      geom_path(aes(x = days_since_20, y = cases, color = state))+
+      theme_minimal() +
+      labs(x = "Days Since 20 Cases",
+           y = "Total Cases",
+           color = "State")
   })
 }
 
